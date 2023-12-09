5e417bfbbcc12956d6145b6293397196d4680d06
Elias Xu <flam0799@gmail.com>
2023-12-09 18:19:27 -0500

    ????

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+import path from 'path';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebPackPlugin from "html-webpack-plugin";
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                type: "asset/resource",
+            },
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./src/componets/index.css",
+            chunkFilename: "index.css"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./src/componets/Nav.css",
+            chunkFilename: "Nav.css"
+        }),
+    ]
+};
