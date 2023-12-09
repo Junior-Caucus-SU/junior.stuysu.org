568358709d4993e66b503c70dc1f19481d52d967
Elias Xu <flam0799@gmail.com>
2023-12-09 18:10:51 -0500

    add work

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Nav.css';
 import StuyLogo from "../Group 64.svg";
- 
+
 function NavBar(props) {
     return (<div class="navbar">
         <div class="navbar-header">
@@ -9,10 +9,10 @@ function NavBar(props) {
             <h1>Junior</h1> <h1>Caucus 2023</h1>
         </div>
         <nav className="menu">
-            <a href="/" className={props.page==="Home"?"menu-item current-page" : "menu-item"} >Home</a>
-            <a href="/people" className={props.page==="People"?"menu-item current-page": "menu-item"} >People</a>
-            <a href="/events" className={props.page==="Events"?"menu-item current-page": "menu-item"} >Events</a>
-            <a href="/resources" className={props.page==="Resources"?"menu-item current-page": "menu-item"}>Resources</a>
+            <a href="/" className={props.page === "Home" ? "menu-item current-page" : "menu-item"} >Home</a>
+            <a href="/people" className={props.page === "People" ? "menu-item current-page" : "menu-item"} >People</a>
+            <a href="/events" className={props.page === "Events" ? "menu-item current-page" : "menu-item"} >Events</a>
+            <a href="/resources" className={props.page === "Resources" ? "menu-item current-page" : "menu-item"}>Resources</a>
         </nav>
     </div>)
 }
