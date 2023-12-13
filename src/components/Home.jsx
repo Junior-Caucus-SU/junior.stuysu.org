a6b9348abf110383c118b70f7490bb63358304ab
Elias Xu <flam0799@gmail.com>
2023-12-12 20:58:47 -0500

    start working on backround + formatting (not going to great)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import NavBar from "./NonPage/NavBar";
+import BorderSvg from "../Images/BorderImg.svg";
+import Background from "../Images/BackgroundHome.svg";
+import "./Home.css";
 
 export default function Home() {
-    let homePage = (<>
+    let homePage = (
+    <div className="homepage-div">
         <NavBar {...{ page: "Home" }} />
-    </>);
+        <div className="homepage-schedule"></div>
+        <div className="schedule-specifics">
+            <img src={BorderSvg} className="schedule-border" alt="schedule border" />
+        </div>
+
+    </div>);
     return homePage;
 }
