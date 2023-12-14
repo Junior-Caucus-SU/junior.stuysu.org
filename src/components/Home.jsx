2c1647a1698e99f6d849313c8be58e3ac24cca48
Elias Xu <flam0799@gmail.com>
2023-12-14 10:56:23 -0500

    some tooling around (bad results)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import NavBar from "./NonPage/NavBar";
 import Schedule from "./NonPage/Schedule";
-import BorderSvg from "../Images/BorderImg.svg";
+// import BorderSvg from "../Images/BorderImg.svg";
 import "./Home.css";
 
 export default function Home() {
     let homePage = (
-        <div className="homepage-div">
-            <NavBar {...{ page: "Home" }} />
-            <div className="homepage-schedule">
-                <Schedule />
-            </div>
-            <div className="schedule-detail-box">
-                <div className="schedule-specifics">
-                    <img src={BorderSvg} className="schedule-border" alt="schedule border" />
+            <div className="homepage-div">
+                <NavBar {...{ page: "Home" }} />
+                <div className="homepage-schedule">
+                    <Schedule />
                 </div>
-                <div className="schedule-fill"></div>
+                <div className="schedule-detail-box">
+                    <div className="schedule-specifics">
+                    </div>
+                    {/* <div className="schedule-fill"></div> */}
+                </div>
             </div>
-        </div>
     );
     return homePage;
 }
