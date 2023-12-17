78068e91c6c2673efea1eda66b6f229dd8e032d5
Otzar Jaffe <otzarjaffe@gmail.com>
2023-12-17 09:30:43 -0500

    presidents side bar

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -4,29 +4,34 @@ import Texture from "./NonPage/Texture";
 import Footer from "./NonPage/Footer";
 import "./People.css";
 
-export default function People(){
-    let peoplePage = (
-        <div>
-            <Navbar page="People"/>
-            <Texture/>
-            <div className="meet-team-container">
-                <div className="meet">Meet</div>
-                <div className="the-team">The Team</div>
-            </div>
-            <div className="team-members">
-                <div className="member">
-                    <div className="image-placeholder"></div>
-                    <div className="name">Josephine</div>
-                    <div className="description">Description here...</div>
-                </div>
-                <div className="member">
-                    <div className="image-placeholder"></div>
-                    <div className="name">Grace</div>
-                    <div className="description">Description here...</div>
-                </div>
-            </div>
-            <Footer/>
+export default function People() {
+  let peoplePage = (
+    <div>
+      <Navbar page="People"/>
+      <Texture/>
+      <div className="meet-team-container">
+        <div className="meet">Meet</div>
+        <div className="the-team">The Team</div>
+      </div>
+      <div className="presidents">
+        <div className="presidents-box">
+          <div className="presidents-box-text">Presidents</div>
         </div>
-    );
-    return peoplePage;
+        <div className="presidents-container">
+          <div className="president">
+            <div className="image-placeholder"></div>
+            <div className="name">Josephine</div>
+            <div className="description">Description here...</div>
+          </div>
+          <div className="president">
+            <div className="image-placeholder"></div>
+            <div className="name">Grace</div>
+            <div className="description">Description here...</div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  );
+  return peoplePage;
 }
