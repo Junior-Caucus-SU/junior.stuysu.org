222699c85dadd69545baf95070b3ccc23845e6a7
Otzar Jaffe <otzarjaffe@gmail.com>
2023-12-17 08:43:36 -0500

    started work on the people page

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import Navbar from "./NonPage/NavBar";
 import Texture from "./NonPage/Texture";
 import Footer from "./NonPage/Footer";
-
+import "./People.css";
 
 export default function People(){
     let peoplePage = (
         <div>
-            <Navbar {...{page:"People"}}/>
+            <Navbar page="People"/>
             <Texture/>
+            <div className="meet-team-container">
+                <div className="meet">Meet</div>
+                <div className="the-team">The Team</div>
+            </div>
+            <div className="team-members">
+                <div className="member">
+                    <div className="image-placeholder"></div>
+                    <div className="name">Josephine</div>
+                    <div className="description">Description here...</div>
+                </div>
+                <div className="member">
+                    <div className="image-placeholder"></div>
+                    <div className="name">Grace</div>
+                    <div className="description">Description here...</div>
+                </div>
+            </div>
             <Footer/>
         </div>
     );
