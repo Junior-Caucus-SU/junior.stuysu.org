d3b0d821af62b3cba3f73e171d08f2d7a4eea451
Connor Yau <cyau50@stuy.edu>
2023-12-31 12:41:26 -0500

    better parallax

--- src/components/NonPage/HomeParallax.jsx
+++ src/components/NonPage/HomeParallax.jsx
@@ -1,14 +1,20 @@
 import Homecoming from '../../Images/homecoming.png';
 import GloriousLeaders from '../../Images/gloriousLeaders.png';
 import ParallaxBackground from '../../Images/parallaxBackgound.png';
+import Battle from '../../Images/battle.png';
+import PhotoCapture from '../../Images/photoCapture.png'
 import React from 'react';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const images = [
   { url: Homecoming, size: 0.5, alt:"homecoming image" },
   { url: GloriousLeaders, size: 2, alt:"polaroid josephine, grace, ryan" },
+  { url: Battle, size: 2.5, alt:"battle" },
+  { url: PhotoCapture, size: 3, alt:"photocapture" },
   // Add more images with their respective sizes
 ];
+
+const offsets = [50, 25, 75, 36];
 
 const backgroundStyle = {
   backgroundImage: ParallaxBackground,
@@ -30,18 +36,24 @@ const ParallaxImages = () => {
             <ParallaxLayer
                 offset={0}
                 speed={1}
-                factor={images.length + 1}
+                factor={images.length * 0.25}
                 style={{
                     backgroundImage: `url(${ParallaxBackground})`,
                     backgroundSize: 'cover',
+                    height: '100vh',
                 }}
             />
             {images.map((image, index) => (
-                <ParallaxLayer offset = {index * 0.5} speed={image.size * 0.5}>
+                <ParallaxLayer offset = {index * 0.25 + 0.1} speed={image.size * 0.5} style={{justifyContent: 'flex-end'}}>
                     <img
                         src = {image.url}
                         alt = {image.alt}
-                        style={{ width: '50%', height: 'auto'}}
+                        style={{
+                            position:'absolute', 
+                            width: '25%', 
+                            height: 'auto', 
+                            right: `${offsets[index]}%`,
+                        }}
                     />
                 </ParallaxLayer>
             ))}
