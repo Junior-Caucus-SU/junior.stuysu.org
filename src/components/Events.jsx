718a4ed83aa92f6f9744b12324c80555c68562df
Tony Chen <tchen51@stuy.edu>
2023-12-30 22:30:05 -0500

    partially working carousel

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,11 +5,13 @@ import Initiatives from "./NonPage/Initiatives";
 import Footer from "./NonPage/Footer"
 import AnnouncementLine from "../Images/announcementsLine.svg"
 import "./Events.css";
+import Carousel from "./NonPage/Carousel";
 
 export default function Events() {
     let eventsPage = (
         <div className="events-page">
             <Navbar {...{ page: "Events" }} />
+            <Carousel/>
             <div className="events-body">
                 <div className="all-initiatives-header">
                         <h1 className="title">All Initiatives</h1>
---
+++ src/components/NonPage/Carousel.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import "./Carousel.css";
+import CarouselImageOne from "../../Images/CarouselImage1.png";
+import CarouselImageTwo from "../../Images/CarouselImage2.png";
+
+
+function CurrentItem({direction}) {
+    if (direction == 0) {
+        return (
+            <div className="current-item" style={{ transform: `translateX(0%)` }}>
+                <img className="images" src={CarouselImageOne} alt=""/>
+                <div className="image-text">Homecoming<br/>Fundraiser</div>
+                <img className="images" src={CarouselImageTwo} alt=""/>
+            </div>
+        );
+    }
+    return (
+        <div className="current-item" style={{ transform: `translateX(-100%)` }}>
+            <img className="images" src={CarouselImageOne}/>
+            <div className="image-text">Homecoming<br/>Fundraiser</div>
+            <img className="images" src={CarouselImageTwo} />
+        </div>
+    );
+}
+
+const Carousel = () => {
+    const [scrollDirection, setScrollDirection] = useState(0);
+    const [prevScrollY, setPrevScrollY] = useState(1);
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+    
+        if (currentScrollY > prevScrollY) {
+          setScrollDirection(1);
+
+        } else if (currentScrollY < prevScrollY) {
+          setScrollDirection(0);
+        }
+    
+        setPrevScrollY(currentScrollY);
+      };
+    
+      useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, [prevScrollY]);
+
+    return (
+        <div className="carousel">
+             <CurrentItem direction={scrollDirection}/>
+        </div>)};
+
+export default Carousel;
+
