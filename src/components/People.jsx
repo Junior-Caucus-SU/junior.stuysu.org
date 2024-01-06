1ea11fbd0192a6834d6304d9e99e1ba5fd00a3c8
Elias Xu <flam0799@gmail.com>
2024-01-06 18:05:07 -0500

    add my bio

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -257,7 +257,9 @@ export default function People() {
                   <div className="d_type">I/T</div>
                   <img src={Elias_Xu} alt="" className="director-img" />
                   <div className="name">Elias Xu</div>
-                  <div className="description">Lorem ipsum dolor sit amet consectetur. Velit convallis sit odio orci varius eu elementum. Est sit et hendrerit id eget diam. Maecenas amet sed ac turpis facilisis semper at. Tincidunt egestas vulputate sed lobortis hac augue. Est pharetra aliquet faucibus.</div>
+                  <div className="description">
+                    Hi! I’m Elias! I’m a beekeeper, book enthusiast, and programmer. Even though I mainly come from a background in Artificial Intelligence and Competitive Programming, I’ve been deemed competent enough to run (with Will Zhang) the I/T department. Cheers!
+                  </div>
                 </div>
                 <div className="director person-container to-fly-from-right">
                   <img src={Will} alt="" className="director-img" />
