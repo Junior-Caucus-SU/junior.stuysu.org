b5d003b2f41fe8d47c572c20fa57b627d1c653b6
Elias Xu <flam0799@gmail.com>
2024-01-11 22:01:38 -0500

    revamp css for footer

--- src/components/NonPage/Footer.jsx
+++ src/components/NonPage/Footer.jsx
@@ -20,36 +20,37 @@ export default function Footer() {
                         </a>
                     </div>
                 </div>
-                <div className="credits">
-                    <div className="positions">
-                        Design Lead<br />
-                        Development Lead<br />
-                        Website Team<br />
-                        Illustration<br />
-                        Special Thanks
-                    </div>
-                    <div className="names">
-                        <a href="https://github.com/willpill" className="name-link will-zhang">Will Zhang</a><br />
-                        <a href="https://github.com/Elias2660" className="name-link elias-xu">Elias Xu</a><br />
-                        <a href="https://github.com/yellowstonepark" className="name-link otzar-jaffe">Otzar Jaffe</a>, <a href="https://github.com/connor132435" className="name-link connor-yau">Connor Yau</a>, <a href="https://github.com/muff1n0" className="name-link tony-chen">Tony Chen</a><br />
-                        Chloe Wong<br />
-                        David Chen
-                    </div>
-                </div>
-                < div className="leadership">
-                    The Junior Caucus is led by Josephine Yoo & Grace Rhee. </ div>
-
-                <div className="leadership">
-                    With gratitudes to chiefs of staff John Jay Wang & Ethan Sie.
-                </div>
-                <a href="https://stuy.enschool.org/" className="school-logo-link">
-                    <div className="school">
-                        <img className="school-icon" src={Stuyvesant} alt="Stuyvesant" />
-                        <div className="school-text">Stuyvesant High School</div>
-                    </div>
-                </a>
-
             </div>
-        </div >
+            <div className="names">
+                <div className="footer-people">
+                    <span className="positions-title">Design Lead</span>  <a href="https://github.com/willpill" className="name-link will-zhang">Will Zhang</a>
+                </div>
+                <div className="footer-people">
+                    <span className="positions-title">Development Lead</span>  <a href="https://github.com/Elias2660" className="name-link elias-xu">Elias Xu</a>
+                </div>
+                <div className="footer-people">
+                    <span className="positions-title">Website Team</span> <div><a href="https://github.com/yellowstonepark" className="name-link otzar-jaffe">Otzar Jaffe</a>, <a href="https://github.com/connor132435" className="name-link connor-yau">Connor Yau</a>, <a href="https://github.com/muff1n0" className="name-link tony-chen">Tony Chen</a><br />
+                    </div></div>
+                <div className="footer-people">
+                    <span className="positions-title">Illustration</span> Chloe Wong
+                </div>
+                <div className="footer-people">
+                    <span className="positions-title">Special Thanks</span> David Chen
+                </div>
+            </div>
+            < div className="leadership">
+                The Junior Caucus is led by Josephine Yoo & Grace Rhee. </ div>
+
+            <div className="leadership">
+                With gratitude to chiefs of staff John Jay Wang & Ethan Sie.
+            </div>
+            <a href="https://stuy.enschool.org/" className="school-logo-link">
+                <div className="school">
+                    <img className="school-icon" src={Stuyvesant} alt="Stuyvesant" />
+                    <div className="school-text">Stuyvesant High School</div>
+                </div>
+            </a>
+
+        </div>
     )
 }
