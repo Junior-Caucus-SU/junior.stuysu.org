c03611db3c17e4a31d7b62ad6b9813fefb805711
willpill <65963376+willpill@users.noreply.github.com>
2024-01-28 09:04:41 -0500

    Fix resources design

--- src/components/NonPage/ResourceCard.jsx
+++ src/components/NonPage/ResourceCard.jsx
@@ -7,28 +7,14 @@ export default function ResourceCard(props) {
         <div className="resource-card-super">
             <a href={props.link} className="resource-links">
                 <div className="resource-card-head">
-                    <h1 className="resource-title resource-title">{props.title}</h1>
-                </div>
-                <p className="resourcecard-description resource-text">
-                    {props.description}
-                </p>
-                <div className="content">
-                    <div>
-                        <p className="resource-text">Release Date</p>
-                        <p className=" resource-text">
+                    <h1 className="resource-title">{props.title}</h1>
+                    <p className=" resource-text">
                             {props.release}
-                        </p>
-                    </div>
-                    <a href={props.link}>
-                        <div className="resource-button">
-                            <span className="resource-text">
-                                Check out Resource
-                            </span>
-                        </div>
-                    </a>
+                    </p>
+                </div>
+                <div className="content">
                 </div>
             </a>
         </div>
-
     );
 }
