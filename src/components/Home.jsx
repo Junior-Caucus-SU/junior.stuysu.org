010d697df925bba112d87459425c6637c953f7b8
Restyled.io <commits@restyled.io>
2024-03-04 01:33:54 +0000

    Restyled by prettier

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -122,33 +122,38 @@ export default function Home() {
   useEffect(() => {
     // parallax effect
     const handleParallax = (entries) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           // speed of the parallax effect
           const backgroundSpeedFactor = 0.5;
 
-          window.addEventListener('scroll', () => {
+          window.addEventListener("scroll", () => {
             // get window height
             let windowHeight = window.innerHeight;
 
-            const scrolled = window.scrollY - entry.target.offsetTop - windowHeight / 2;
+            const scrolled =
+              window.scrollY - entry.target.offsetTop - windowHeight / 2;
 
             // background position
-            document.querySelector('.polaroids-sky').style.backgroundPositionY = `-${scrolled * backgroundSpeedFactor}px`;
-            document.querySelector('.polaroids-bottom').style.backgroundPositionY = `-${scrolled * backgroundSpeedFactor}px`;
+            document.querySelector(".polaroids-sky").style.backgroundPositionY =
+              `-${scrolled * backgroundSpeedFactor}px`;
+            document.querySelector(
+              ".polaroids-bottom",
+            ).style.backgroundPositionY =
+              `-${scrolled * backgroundSpeedFactor}px`;
           });
         }
       });
     };
 
-    const observer = new IntersectionObserver(handleParallax, { threshold: 0.1 });
+    const observer = new IntersectionObserver(handleParallax, {
+      threshold: 0.1,
+    });
 
     //element to observe
-    const target = document.querySelector('.end-scroll');
+    const target = document.querySelector(".end-scroll");
     observer.observe(target);
   }, []);
-
-
 
   const dayInfo = {
     dayType,
@@ -302,7 +307,8 @@ const getPeriodTimes = (DayType) => {
         end.setHours(parseInt(start.split(":")[0]));
         end.setMinutes(parseInt(start.split(":")[1]) + info[i].duration);
         final.push(
-          `${start} - ${end.getHours()}:${end.getMinutes() < 10 ? "0" + end.getMinutes() : end.getMinutes()
+          `${start} - ${end.getHours()}:${
+            end.getMinutes() < 10 ? "0" + end.getMinutes() : end.getMinutes()
           }`,
         );
       }
